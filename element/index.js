f016ba19966d892ec5b643e1f896a9cb3a41ae43
John Osterman <josterman@pewtrusts.org>
2019-05-16 15:47:16 -0400

    Adds child.resolve(true) to Element so that child components resolve after waiting on their parent to resolve befor being added to DOM

--- element/index.js
+++ element/index.js
@@ -16,13 +16,16 @@ export default class Element {
         this.data = options.data;
         this.createComponent = options.createComponent || null; // null option for backward compatibility. previous version don't send this property
         this.el = this.prerender(arguments); // will call the instance's prerender
+        console.log(this.name);
         this.isReady = new Promise((resolve, reject) => {
             this.resolve = resolve;
             this.reject = reject;
         }).then(() => {
             this.children.forEach(child => {
+                console.log(this.name, child);
                 child.container = $d.q(child.renderToSelector);
                 child.container.appendChild(child.el);
+                child.resolve(true);
             });
         });
         if ( !this.prerendered && this.container ){ // if is not prerendered and container already exists
